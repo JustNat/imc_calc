61853b4124da6c088a92948ca4e9eef45cab16fe
Gabriel Pereira <pereira.gabriel@ADDIVERSA.LOCAL>
2023-10-06 17:39:32 -0300

    feat: classe pessoa e seus getters e setters

---
+++ lib/Classes/person.dart
@@ -0,0 +1,29 @@
+class Person {
+  String _name = '';
+  double _weight = 0;
+  double _height = 0;
+
+  String getName() {
+    return _name;
+  }
+
+  void setName(String name) {
+    _name = name;
+  }
+
+  double getWeight() {
+    return _weight;
+  }
+
+  void setWeight(double weight) {
+    _weight = weight;
+  }
+
+  double getHeight() {
+    return _height;
+  }
+
+  void setHeight(double height) {
+    _height = height;
+  }
+}
